c6acb71647049b5da63dcdf1df430d97797a58d5
8bu <nhvlongg@gmail.com>
2022-04-28 22:32:49 +0700

    correct typing

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,7 @@ export type Component = ReturnType<typeof defineComponent>
 export type DestroyFn = () => void
 export interface MountFnConfig {
   el: Component
-  props?: Ref<VNodeProps & Record<string, any>>
+  props?: Ref<VNodeProps & Record<string, any>> | VNodeProps & Record<string, any>
   elContent?: ChildFnConfig[]
   parent?: Component
 }
